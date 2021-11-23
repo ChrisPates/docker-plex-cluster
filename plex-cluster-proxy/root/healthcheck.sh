d45d80618b8dc877ff466f4ebd80fb33d114d3ea
elcrp96 <ChrisPates@users.noreply.github.com>
2021-11-23 16:58:57 +0000

    fixes
    moved nginx config out on bind mounted folders to allow for the use of portainer edge stacks

--- plex-cluster-proxy/root/healthcheck.sh
+++ plex-cluster-proxy/root/healthcheck.sh
@@ -3,5 +3,5 @@
 TARGET=localhost
 CURL_OPTS="--connect-timeout 15 --max-time 100 --silent --show-error --fail"
 
-curl ${CURL_OPTS} "http://${TARGET}:${HTTPS_PORT}/identity" >/dev/null
+curl ${CURL_OPTS} "http://127.0.0.1:32300/identity" >/dev/null
 
